8b6c14c764913526556973f732aa2dc41a820175
Michael Singhurse <michaelsinghurse@gmail.com>
2020-04-21 17:14:46 -0500

    Deployment

--- schema.sql
+++ schema.sql
@@ -1,18 +1,21 @@
 CREATE TABLE todolists (
-id serial PRIMARY KEY,
-title text NOT NULL UNIQUE
-username text NOT NULL
+  id serial PRIMARY KEY,
+  title text NOT NULL UNIQUE,
+  username text NOT NULL
 );
 
 CREATE TABLE todos (
-id serial PRIMARY KEY,
-title text NOT NULL,
-done boolean NOT NULL DEFAULT false,
-username text NOT NULL,
-todolist_id int NOT NULL REFERENCES todolists (id) ON DELETE CASCADE
+  id serial PRIMARY KEY,
+  title text NOT NULL,
+  done boolean NOT NULL DEFAULT false,
+  username text NOT NULL,
+  todolist_id integer
+    NOT NULL
+    REFERENCES todolists (id)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE users (
-username text PRIMARY KEY,
-password text NOT NULL
+  username text PRIMARY KEY,
+  password text NOT NULL
 );
